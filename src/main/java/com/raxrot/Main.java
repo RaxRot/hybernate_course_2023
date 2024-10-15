dc87200aa8fe806d88183e7e0b40c41f774c4053
Vlad Bulahov <dasistperfektosss@gmail.com>
2024-10-15 23:40:34 +0100

    add two type of connection xml and class add Entity and test insert

--- src/main/java/com/raxrot/Main.java
+++ src/main/java/com/raxrot/Main.java
@@ -1,7 +1,39 @@
 package com.raxrot;
 
+import com.raxrot.entities.Product;
+import com.raxrot.persistence.CustomPersistenceUnitInfo;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.jpa.HibernatePersistenceProvider;
+
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //EntityManagerFactory emf= Persistence.createEntityManagerFactory("my-persistence-unit");
+        EntityManager em;
+        try (EntityManagerFactory emf = new HibernatePersistenceProvider()
+                .createContainerEntityManagerFactory(new CustomPersistenceUnitInfo(), new HashMap<>())) {
+            em = emf.createEntityManager();
+        }
+
+        try {
+           em.getTransaction().begin();
+
+           Product p = new Product();
+           p.setId(2L);
+           p.setName("Chocolate");
+           em.persist(p);//register object
+
+           em.getTransaction().commit();//insert
+       }catch (Exception e) {
+           if (em.getTransaction().isActive()) {
+               em.getTransaction().rollback();
+           }
+           e.printStackTrace();
+       }finally {
+           em.close();
+       }
     }
 }
